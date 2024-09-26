90e36212ea51cbb0ad2aec183a4b4c2035e2acf2
Gabor12 <nagyg1@kkszki.hu>
2024-09-26 10:17:02 +0000

    Connect class ready to use

---
+++ Connect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSale
+{
+    Mysql
+    public class Connect
+    {
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+    }
+}
